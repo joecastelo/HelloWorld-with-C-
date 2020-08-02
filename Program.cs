ea3efac8741af6b52c351d80999d792ad330e6fc
joecastelo <jhmcastelo@gmail.com>
2020-08-02 20:34:52 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World";
+            Console.WriteLine(message);
+            MessageBox.Show(message);
+            Patient jack = new Patient("123", 56, 1);
+            jack.CallPatient();
+        }
+    }
+    class Patient
+    {
+        public string ID { get; set; }
+        public int Age { get; set; }
+        public int Priority { get; set; }
+
+        public Patient(string iD, int age, int priority)
+        {
+            ID = iD;
+            Age = age;
+            Priority = priority;
+        }
+        public void CallPatient()
+        {
+            MessageBox.Show(ID + " " + Age + " " + Priority);
+        }
+    }
+
+
+}
